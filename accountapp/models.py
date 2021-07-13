4e4ef718eaf27785a058c6ac6070263a44fb5f2f
youngjuj <alti100@likelion.org>
2021-07-14 00:18:47 +0900

    21/07/13 me_alone login&logout

--- accountapp/models.py
+++ accountapp/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class HelloWorld(models.Model):
-    text=models.CharField(max_length=255, null=False)
+    text = models.CharField(max_length=255, null=False)
--- accountapp/views.py
+++ accountapp/views.py
@@ -30,5 +30,5 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:hello_newworld')
     template_name = 'accountapp/create.html'
