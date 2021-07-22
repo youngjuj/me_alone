4457f0e73b0c8f2c6f88ca50aece4e5071cc1c98
youngjuj <alti100@likelion.org>
2021-07-22 12:30:36 +0900

    21/07/22 me_alone medai + error hold

--- me_alone/settings.py
+++ me_alone/settings.py
@@ -141,6 +141,10 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
