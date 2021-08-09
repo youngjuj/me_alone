ef2ae38bb5911929b2a24450051fc999fec8ddab
youngjuj <alti100@likelion.org>
2021-08-09 11:50:35 +0900

    21/08/09 create comment

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,2 +1,9 @@
+from django.urls import path
 
-urlpatterns=[]
+from commentapp.views import CommentCreateView
+
+app_name = 'commentapp'
+
+urlpatterns = [
+    path('create/', CommentCreateView.as_view(), name='create'),
+]
