b8bb7140e0589af044a514e97d4f53d12bc30852
youngjuj <alti100@likelion.org>
2021-09-13 12:53:51 +0900

    21/9/13 mariadb

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'me_alone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'me_alone.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
